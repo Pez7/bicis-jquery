44638385f8294e8458958ad2f22e3e447fafee39
Maria Paz <maria.pazrojasc@gmail.com>
2017-07-09 11:57:41 -0400

    validación nombre

--- js/main.js
+++ js/main.js
@@ -3,4 +3,21 @@ function validateForm(){
 	var apellido = $('#lastname').val();
 	var email = $('#input-email').val();
 	var password = $('#input-password').val();
+	var opciones = $('#options').val();
+	var caja1 = $('.input-box')[0];
+	var caja2 = $('.input-box')[1];
+	var caja3 = $('.input-box')[2];
+	var caja4 = $('.input-box')[3];
+	var caja5 = $('.input-box')[4];
+
+if (nombre.charAt(0) != nombre.charAt(0).toUpperCase() || nombre.length == 0 || nombre == ""){
+
+	$(caja1).append('<span>Ingresa un nombre válido</span>');
+
+	return caja1;
+	}
+
+
+
+
 }
